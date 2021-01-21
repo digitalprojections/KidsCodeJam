5df19a6eb5a222c975003ebd4728ce7543a06ab4
maxShinobi <mxshnb@gmail.com>
2021-01-21 19:36:14 +0900

    settings menu
    audio mixer controllers and script done

--- Assets/Scripts/settings.cs
+++ Assets/Scripts/settings.cs
@@ -1,8 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class settings : MonoBehaviour
 {
-        
+    public AudioMixer musicAudioMixer;
+    public AudioMixer fxAudioMixer;
+    public void SetMusicVolume (float volume)
+    {
+        musicAudioMixer.SetFloat("musicVolume", volume);
+    }
+
+    public void SetFXVolume (float volume)
+    {
+        fxAudioMixer.SetFloat("fxVolume", volume);
+    }
+
 }
